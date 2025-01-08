77951e0261563bac309346f6fb102dbd7b2102ad
Vidhul S <vidhuls1003@gmail.com>
2025-01-08 16:02:28 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main (String args[]){
+        Player p1 = new Player("Vid",4);
+        Player p2 = new Player("Lay",2);
+        Player p3= new Player();
+        p3.setName("Dud");
+        p3.setRank(3);
+        p1.printInfo();
+        p2.printInfo();
+        p3.printInfo();
+        Player.printCount();
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -28,15 +28,4 @@ public class Player {
     public static void printCount(){
         System.out.println(playerCount);
     }
-    public static void main (String args[]){
-        Player p1 = new Player("Vid",4);
-        Player p2 = new Player("Lay",2);
-        Player p3= new Player();
-        p3.setName("Dud");
-        p3.setRank(3);
-        p1.printInfo();
-        p2.printInfo();
-        p3.printInfo();
-        printCount();
-    }
 }
