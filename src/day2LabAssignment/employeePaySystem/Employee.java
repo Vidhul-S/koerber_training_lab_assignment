58ef840da7da7f77b8c44a46a122b955b0676864
Vidhul S <vidhuls1003@gmail.com>
2025-01-13 00:05:38 +0530

    commit

---
+++ src/day2LabAssignment/employeePaySystem/Employee.java
@@ -0,0 +1,48 @@
+package day2LabAssignment.employeePaySystem;
+
+public class Employee {
+
+    public int id;
+    public String firstName;
+    public String lastName;
+    public double salary;
+    public Employee(int id, String firstName, String lastName, double salary){
+
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+    public int getID(){
+
+        return this.id;
+    }
+    public String getFirstName(){
+
+        return new String(this.firstName);
+    }
+    public String getLastName(){
+
+        return new String(this.lastName);
+    }
+    public double getSalary(){
+
+        return this.salary;
+    }
+    public void setSalary(double salary){
+
+        this.salary=salary;
+    }
+    public double getAnnualSalary(){
+
+        return (this.salary)*365;
+    }
+    public double raiseSalary(int percent){
+        this.salary += this.salary*percent/100;
+        return this.salary;
+    }
+    @Override
+    public String toString(){
+        return "Employee[id="+this.id+",name="+this.firstName+this.lastName+",salary="+this.salary;
+    }
+}
