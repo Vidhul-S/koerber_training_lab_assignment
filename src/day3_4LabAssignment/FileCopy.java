4dec1de13f7a8d591cd57b4883bb3f295a2359a6
Vidhul S <vidhuls1003@gmail.com>
2025-01-13 12:47:05 +0530

    commit

---
+++ src/day3_4LabAssignment/FileCopy.java
@@ -0,0 +1,35 @@
+package day3_4LabAssignment;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+public class FileCopy {
+
+    public static void main(String[] args) throws java.io.IOException {
+
+        long startTime = System.currentTimeMillis();
+        try(FileInputStream fi = new FileInputStream("C:\\Users\\vidhul.s\\IdeaProjects\\M4X.avif");
+        FileOutputStream fo = new FileOutputStream("C:\\Users\\vidhul.s\\IdeaProjects\\Koreber Training\\src\\day3_4LabAssignment\\M4X.avif");) {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fi.read(buffer)) != -1) {
+
+                fo.write(buffer, 0, bytesRead);
+            }
+            System.out.println("File copied successfully (binary).");
+        } catch (Exception e) {
+
+            throw new RuntimeException(e);
+        }
+        finally {
+
+            long stopTime = System.currentTimeMillis();
+            System.out.println("Execution time: " + (stopTime - startTime) + " milliseconds.");
+        }
+
+    }
+
+}
---
+++ src/day3_4LabAssignment/FileCopyBufferWay.java
@@ -0,0 +1,32 @@
+package day3_4LabAssignment;
+
+import java.io.*;
+
+public class FileCopyBufferWay {
+
+    public static void main(String[] args) throws java.io.IOException {
+
+        long startTime = System.currentTimeMillis();
+        try(BufferedInputStream fi = new BufferedInputStream(new FileInputStream("C:\\Users\\vidhul.s\\IdeaProjects\\M4X.avif"));
+            BufferedOutputStream fo = new BufferedOutputStream(new FileOutputStream("C:\\Users\\vidhul.s\\IdeaProjects\\Koreber Training\\src\\day3_4LabAssignment\\M4X.avif"));) {
+
+            byte[] buffer = new byte[1024]; // Buffer to hold the data
+            int bytesRead;
+            while ((bytesRead = fi.read(buffer)) != -1) {
+                fo.write(buffer, 0, bytesRead);
+            }
+
+            System.out.println("File copied successfully (binary).");
+        } catch (Exception e) {
+
+            throw new RuntimeException(e);
+        }
+        finally {
+
+            long stopTime = System.currentTimeMillis();
+            System.out.println("Execution time: " + (stopTime - startTime) + " milliseconds.");
+        }
+    }
+}
+
+
